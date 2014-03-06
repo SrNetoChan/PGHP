282bb15dbfbef0261ef79d4a234626bf104354a0
SrNetoChan <senhor.neto@gmail.com>
2014-03-06 10:15:28 +0000

    alterações ao id-->gid

--- alternativa_estruturas_inherit+timetracker.sql
+++ alternativa_estruturas_inherit+timetracker.sql
@@ -4,7 +4,7 @@ TRUNCATE TABLE "PGHP".infraestruturas
 
 CREATE TABLE "PGHP".infraestruturas
 (
-	infra_oid integer not null,
+	gid serial,
 	uniterr_oid integer REFERENCES "PGHP".unidadesterritoriais(oid),
 	classe character varying(40),
 	tipo character varying(40),
@@ -25,7 +25,7 @@ CREATE INDEX infraestruturas_idx
   USING gist
   (geom);
 
--- criar indices para a class, para o infra_oid
+-- criar indices para a class, para o gid
 -- criar indices para time_start e time_stop? 
 
 CREATE TABLE "PGHP".infraestruturas_pontos
@@ -45,17 +45,11 @@ CREATE INDEX infraestruturas_pontos_idx
   (geom);
 
 
---ALTER TABLE "PGHP".infraestruturas_pontos
---    ALTER COLUMN geom TYPE geometry(POINT,3763) USING geom;
-
---UPDATE geometry_columns SET type = 'POINT'-
---WHERE f_table_name = "infraestruturas_pontos" AND f_geometry_column = 'geom';
-
 -- CRIAR VERSIONING
 -- View com estado actual da tabela geral das infraestruturas
-CREATE VIEW "PGHP"."infraestruturas_current" AS
+CREATE OR REPLACE VIEW "PGHP"."infraestruturas_current" AS
   SELECT
-    "infra_oid",
+    "gid",
     "uniterr_oid",
     "classe",
     "tipo",
@@ -65,18 +59,16 @@ CREATE VIEW "PGHP"."infraestruturas_current" AS
     "accao",
     "observacoes",
     "geom",
-    "id_hist",
     "time_start",
-    "time_end",
     "user_update"
   FROM "PGHP"."infraestruturas"
   WHERE "time_end" IS NULL;
 
 -- função para ver tabela em determinado dia/hora (usado apenas pela administração para resolver problemas)
-CREATE OR REPLACE FUNCTION "PGHP"."infraestruturas_at_time"(timestamp)
+CREATE OR REPLACE FUNCTION "PGHP"."infraestruturas_at_time"(timestamp without time zone)
 RETURNS SETOF "PGHP"."infraestruturas_current" AS
 $$
-SELECT "infra_oid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","id_hist","time_start","time_end","user_update" FROM "PGHP"."infraestruturas" WHERE
+SELECT "gid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","time_start","user_update" FROM "PGHP"."infraestruturas" WHERE
   ( SELECT CASE WHEN "time_end" IS NULL THEN ("time_start" <= $1) ELSE ("time_start" <= $1 AND "time_end" > $1) END );
 $$
 LANGUAGE 'sql';
@@ -87,7 +79,6 @@ RETURNS TRIGGER AS
 $$
 DECLARE
 	var_geomtype text;
-	var_table text;
 BEGIN
   IF OLD."time_end" IS NOT NULL THEN
     RETURN NULL;
@@ -95,14 +86,14 @@ BEGIN
   IF NEW."time_end" IS NULL THEN
     var_geomtype := geometrytype(NEW.geom);
     IF var_geomtype IN ('MULTIPOLYGON', 'POLYGON') THEN
-      INSERT INTO "PGHP"."infraestruturas_poligonos" ("infra_oid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","id_hist","time_start","time_end","user_update")
-      VALUES (OLD."infra_oid",OLD."uniterr_oid",OLD."classe",OLD."tipo",OLD."nome",OLD."cadeado",OLD."estado",OLD."accao",OLD."observacoes",OLD."geom",OLD."id_hist",OLD."time_start",current_timestamp,user);
+      INSERT INTO "PGHP"."infraestruturas_poligonos" ("gid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","time_start","time_end","user_update")
+      VALUES (OLD."gid",OLD."uniterr_oid",OLD."classe",OLD."tipo",OLD."nome",OLD."cadeado",OLD."estado",OLD."accao",OLD."observacoes",OLD."geom",OLD."time_start",current_timestamp,user);
     ELSIF var_geomtype IN ('LINESTRING', 'MULTILINESTRING') THEN
-      INSERT INTO "PGHP"."infraestruturas_linhas" ("infra_oid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","id_hist","time_start","time_end","user_update")
-      VALUES (OLD."infra_oid",OLD."uniterr_oid",OLD."classe",OLD."tipo",OLD."nome",OLD."cadeado",OLD."estado",OLD."accao",OLD."observacoes",OLD."geom",OLD."id_hist",OLD."time_start",current_timestamp,user);
+      INSERT INTO "PGHP"."infraestruturas_linhas" ("gid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","time_start","time_end","user_update")
+      VALUES (OLD."gid",OLD."uniterr_oid",OLD."classe",OLD."tipo",OLD."nome",OLD."cadeado",OLD."estado",OLD."accao",OLD."observacoes",OLD."geom",OLD."time_start",current_timestamp,user);
     ELSE
-      INSERT INTO "PGHP"."infraestruturas_pontos" ("infra_oid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","id_hist","time_start","time_end","user_update")
-      VALUES (OLD."infra_oid",OLD."uniterr_oid",OLD."classe",OLD."tipo",OLD."nome",OLD."cadeado",OLD."estado",OLD."accao",OLD."observacoes",OLD."geom",OLD."id_hist",OLD."time_start",current_timestamp,user);
+      INSERT INTO "PGHP"."infraestruturas_pontos" ("gid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","time_start","time_end","user_update")
+      VALUES (OLD."gid",OLD."uniterr_oid",OLD."classe",OLD."tipo",OLD."nome",OLD."cadeado",OLD."estado",OLD."accao",OLD."observacoes",OLD."geom",OLD."time_start",current_timestamp,user);
     END IF;
     NEW."time_start" = current_timestamp;
   END IF;
@@ -111,7 +102,7 @@ END;
 $$
 LANGUAGE 'plpgsql';
 
--- REVER PARA CONSIDERAR a alteração do infra_oid
+-- REVER PARA CONSIDERAR a alteração do gid
 CREATE OR REPLACE FUNCTION "PGHP"."infraestruturas_insert"()
 RETURNS trigger AS
 $$
@@ -125,16 +116,6 @@ BEGIN
 END;
 $$
 LANGUAGE 'plpgsql';
-
--- RECRIAR ESTES TRIGGERS PARA CADA TABELA CHILD DE PONTOS, LINHAS OU POLIGONOS
-CREATE RULE "infraestruturas_del" AS ON DELETE TO "PGHP"."infraestruturas"
-DO INSTEAD UPDATE "PGHP"."infraestruturas" SET "time_end" = current_timestamp, "user_update" = user WHERE "id_hist" = OLD."id_hist" AND "time_end" IS NULL;
-
-CREATE TRIGGER "infraestruturas_update" BEFORE UPDATE ON "PGHP"."infraestruturas"
-FOR EACH ROW EXECUTE PROCEDURE "PGHP"."infraestruturas_update"();
-
-CREATE TRIGGER "infraestruturas_insert" BEFORE INSERT ON "PGHP"."infraestruturas"
-FOR EACH ROW EXECUTE PROCEDURE "PGHP"."infraestruturas_insert"();
 
 -- PONTOS
 CREATE RULE "infraestruturas_pontos_del" AS ON DELETE TO "PGHP"."infraestruturas_pontos"
@@ -167,25 +148,19 @@ CREATE TRIGGER "infraestruturas_insert" BEFORE INSERT ON "PGHP"."infraestruturas
 FOR EACH ROW EXECUTE PROCEDURE "PGHP"."infraestruturas_insert"();
 
 -- CRIAR VIEWS para cada classe
-
 CREATE OR REPLACE VIEW "PGHP".infra_portoes AS
-SELECT	"infra_oid", "uniterr_oid", "tipo", "nome", "cadeado", "estado", "accao", "observacoes", "geom"::Geometry(POINT, 3763), "id_hist", "time_start", "time_end", "user_update"
+SELECT	"gid", "uniterr_oid", "tipo", "nome", "cadeado", "estado", "accao", "observacoes", "geom"::Geometry(POINT, 3763), "time_start", "user_update"
 FROM	"PGHP"."infraestruturas_pontos"
 WHERE	"classe" = 'portoes' AND "time_end" IS NULL;
 
 --CRIAR TRIGGERS E RULES para tornar a VIEW "Editável"
 CREATE OR REPLACE RULE "_DELETE" AS ON DELETE TO "PGHP".infra_portoes DO INSTEAD
-  DELETE FROM "PGHP"."infraestruturas_pontos" WHERE "id_hist" = old."id_hist";
+  DELETE FROM "PGHP"."infraestruturas_pontos" WHERE gid = old.gid;
 CREATE OR REPLACE RULE "_INSERT" AS ON INSERT TO "PGHP".infra_portoes DO INSTEAD
-  INSERT INTO "PGHP"."infraestruturas_pontos" ("infra_oid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom","id_hist","time_start","time_end","user_update") VALUES (NEW."infra_oid",NEW."uniterr_oid",'portoes',NEW."tipo",NEW."nome",NEW."cadeado",NEW."estado",NEW."accao",NEW."observacoes",NEW."geom",nextval('"PGHP".infraestruturas_id_hist_seq'::regclass),NEW."time_start",NEW."time_end",NEW."user_update");
+  INSERT INTO "PGHP"."infraestruturas_pontos" ("gid","uniterr_oid","classe","tipo","nome","cadeado","estado","accao","observacoes","geom")
+    VALUES (NEW."gid",NEW."uniterr_oid",'portoes',NEW."tipo",NEW."nome",NEW."cadeado",NEW."estado",NEW."accao",NEW."observacoes",NEW."geom");
 CREATE OR REPLACE RULE "_UPDATE" AS ON UPDATE TO "PGHP".infra_portoes DO INSTEAD
-  UPDATE "PGHP"."infraestruturas_pontos" SET "infra_oid" = NEW."infra_oid","uniterr_oid" = NEW."uniterr_oid","classe" = 'portoes',"tipo" = NEW."tipo","nome" = NEW."nome","cadeado" = NEW."cadeado","estado" = NEW."estado","accao" = NEW."accao","observacoes" = NEW."observacoes","geom" = NEW."geom","id_hist" = NEW."id_hist","time_start" = NEW."time_start","time_end" = NEW."time_end","user_update" = NEW."user_update" WHERE "id_hist" = NEW."id_hist";
-
-
-
-
-
-
+  UPDATE "PGHP"."infraestruturas_pontos" SET "gid" = NEW."gid","uniterr_oid" = NEW."uniterr_oid","classe" = 'portoes',"tipo" = NEW."tipo","nome" = NEW."nome","cadeado" = NEW."cadeado","estado" = NEW."estado","accao" = NEW."accao","observacoes" = NEW."observacoes","geom" = NEW."geom" WHERE gid = NEW.gid;
 
 
 
