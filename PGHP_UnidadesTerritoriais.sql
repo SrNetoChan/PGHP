1818936bf01998ad576865fc76bdbe792a170260
SrNetoChan <senhor.neto@gmail.com>
2014-03-10 14:56:26 +0000

    Unidades territoriais working

--- PGHP_UnidadesTerritoriais.sql
+++ PGHP_UnidadesTerritoriais.sql
@@ -1,10 +1,12 @@
 ﻿-- UNIDADES TERRITORIAIS --
+--DROP TABLE "PGHP_2".unidadesterritoriais CASCADE;
 
-CREATE TABLE "PGHP".unidadesterritoriais
+
+CREATE TABLE "PGHP_2".unidadesterritoriais
 (
   gid serial primary key,
-  oid SERIAL PRIMARY KEY,
-  nome character varying(50) UNIQUE NOT NULL,
+  oid SERIAL,
+  nome character varying(50) NOT NULL,
   descricao character varying(255),
   time_start timestamp, -- data de criação da linha
   time_end timestamp, -- data de "eliminação" da linha
@@ -15,16 +17,16 @@ CREATE TABLE "PGHP".unidadesterritoriais
 -- Criar versioning --
 
 -- View com estado actual da tabela geral das infraestruturas (i.e. a linha não foi eliminada)
-CREATE OR REPLACE VIEW "PGHP"."unidadesterritoriais_current" AS
+CREATE OR REPLACE VIEW "PGHP_2"."unidadesterritoriais_current" AS
   SELECT "gid", "oid", "nome", "descricao", "time_start", "time_end", "user_update"
-  FROM "PGHP"."unidadesterritoriais"
+  FROM "PGHP_2"."unidadesterritoriais"
   WHERE "time_end" IS NULL;
 
  -- Função para visualizar tabela em determinado dia/hora (usado apenas pela administração para resolver problemas)
-CREATE OR REPLACE FUNCTION "PGHP"."unidadesterritoriais_at_time"(timestamp without time zone)
-RETURNS SETOF "PGHP"."unidadesterritoriais_current" AS
+CREATE OR REPLACE FUNCTION "PGHP_2"."unidadesterritoriais_at_time"(timestamp without time zone)
+RETURNS SETOF "PGHP_2"."unidadesterritoriais_current" AS
 $$
-SELECT "gid", "oid", "nome", "descricao", "time_start", "time_end", "user_update" FROM "PGHP"."infraestruturas" WHERE
+SELECT "gid", "oid", "nome", "descricao", "time_start", "time_end", "user_update" FROM "PGHP_2"."unidadesterritoriais" WHERE
   ( SELECT CASE WHEN "time_end" IS NULL THEN ("time_start" <= $1) ELSE ("time_start" <= $1 AND "time_end" > $1) END );
 $$
 LANGUAGE 'sql';
@@ -33,7 +35,7 @@ LANGUAGE 'sql';
 
 -- Esta função, faz com que quando é pedido para actualizar uma linha na tabela,
 -- em vez disso (ver trigger) arquiva a versão anterior da linha e insere
-CREATE OR REPLACE FUNCTION "PGHP".unidadesterritoriais_update()
+CREATE OR REPLACE FUNCTION "PGHP_2".unidadesterritoriais_update()
   RETURNS trigger AS
 $BODY$
 BEGIN
@@ -41,7 +43,8 @@ BEGIN
     RETURN NULL;
   END IF;
   IF NEW."time_end" IS NULL THEN
-    INSERT INTO "PGHP"."unidadesterritoriais" ("gid","oid","nome","descricao","time_start","time_end","user_update") VALUES (OLD."gid",OLD."nome",OLD."geom", OLD."time_start", current_timestamp,user);
+    INSERT INTO "PGHP_2"."unidadesterritoriais" ("oid","nome","descricao","time_start","time_end","user_update") 
+       VALUES (OLD."oid",OLD."nome",OLD."descricao", OLD."time_start", current_timestamp,user);
     NEW."time_start" = current_timestamp;
   END IF;
   RETURN NEW;
@@ -49,12 +52,12 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION "PGHP".unidadesterritoriais_update()
+ALTER FUNCTION "PGHP_2".unidadesterritoriais_update()
   OWNER TO postgres;
 
 -- Esta função, faz com que quando é pedido para inserir uma linha na tabela,
 -- sejam actualizados os campos de versioning time_start, time_end e user_update
-CREATE OR REPLACE FUNCTION "PGHP".unidadesterritoriais_insert()
+CREATE OR REPLACE FUNCTION "PGHP_2".unidadesterritoriais_insert()
 RETURNS trigger AS
 $$
 BEGIN
@@ -65,7 +68,7 @@ BEGIN
   end if;
   -- se o oid for deixado em branco é-lhe atribuído o próximo número na sequencia
   if NEW."oid" IS NULL then
-    NEW."oid" = nextval('"PGHP".unidadesterritoriais_oid_seq'::regclass);
+    NEW."oid" = nextval('"PGHP_2".unidadesterritoriais_oid_seq'::regclass);
   end if;
   RETURN NEW;
 END;
@@ -73,39 +76,40 @@ $$
 LANGUAGE 'plpgsql';
 
 -- RULES E TRIGGER
-CREATE RULE "unidadesterritoriais_del" AS ON DELETE TO "PGHP"."unidadesterritoriais"
-DO INSTEAD UPDATE "PGHP"."unidadesterritoriais" SET "time_end" = current_timestamp, "user_update" = user WHERE "gid" = OLD."gid" AND "time_end" IS NULL;
+CREATE RULE "unidadesterritoriais_del" AS ON DELETE TO "PGHP_2"."unidadesterritoriais"
+DO INSTEAD UPDATE "PGHP_2"."unidadesterritoriais" SET "time_end" = current_timestamp, "user_update" = user WHERE "gid" = OLD."gid" AND "time_end" IS NULL;
 
-CREATE TRIGGER "unidadesterritoriais_update" BEFORE UPDATE ON "PGHP"."unidadesterritoriais"
-FOR EACH ROW EXECUTE PROCEDURE "PGHP"."unidadesterritoriais_update"();
+CREATE TRIGGER "unidadesterritoriais_update" BEFORE UPDATE ON "PGHP_2"."unidadesterritoriais"
+FOR EACH ROW EXECUTE PROCEDURE "PGHP_2"."unidadesterritoriais_update"();
 
-CREATE TRIGGER "unidadesterritoriais_insert" BEFORE INSERT ON "PGHP"."unidadesterritoriais"
-FOR EACH ROW EXECUTE PROCEDURE "PGHP"."unidadesterritoriais_insert"();
+CREATE TRIGGER "unidadesterritoriais_insert" BEFORE INSERT ON "PGHP_2"."unidadesterritoriais"
+FOR EACH ROW EXECUTE PROCEDURE "PGHP_2"."unidadesterritoriais_insert"();
 
 
 --CRIAR TRIGGERS E RULES para tornar a VIEW "Editável"
-CREATE OR REPLACE RULE "_DELETE" AS ON DELETE TO "PGHP"."unidadesterritoriais_current" DO INSTEAD
-  DELETE FROM "PGHP"."unidadesterritoriais" WHERE gid = OLD."gid";
-CREATE OR REPLACE RULE "_INSERT" AS ON INSERT TO "PGHP"."unidadesterritoriais_current" DO INSTEAD
-  INSERT INTO "PGHP"."unidadesterritoriais" ("oid","nome","descricao")
+CREATE OR REPLACE RULE "_DELETE" AS ON DELETE TO "PGHP_2"."unidadesterritoriais_current" DO INSTEAD
+  DELETE FROM "PGHP_2"."unidadesterritoriais" WHERE gid = OLD."gid";
+CREATE OR REPLACE RULE "_INSERT" AS ON INSERT TO "PGHP_2"."unidadesterritoriais_current" DO INSTEAD
+  INSERT INTO "PGHP_2"."unidadesterritoriais" ("oid","nome","descricao")
     VALUES (NEW."oid",NEW."nome",NEW."descricao");
-CREATE OR REPLACE RULE "_UPDATE" AS ON UPDATE TO "PGHP"."unidadesterritoriais_current" DO INSTEAD
-  UPDATE "PGHP"."unidadesterritoriais_current"
+CREATE OR REPLACE RULE "_UPDATE" AS ON UPDATE TO "PGHP_2"."unidadesterritoriais_current" DO INSTEAD
+  UPDATE "PGHP_2"."unidadesterritoriais"
     SET "oid" = NEW."oid", "nome" = NEW."nome", "descricao" = NEW."descricao" 
     WHERE gid = OLD."gid";
 
 
-
+/*
 -- LIMITES --
 
-CREATE TABLE "PGHP".limites (
+CREATE TABLE "PGHP_2".limites (
   gid SERIAL NOT NULL,
-  uniterr_oid integer references "PGHP".unidadesterritoriais(oid),
+  uniterr_oid integer references "PGHP_2".unidadesterritoriais(oid),
   nome character varying(30),
   geom geometry(MultiPolygon, 3763),
   CONSTRAINT "limites_pkey" PRIMARY KEY (gid));
 
 CREATE INDEX limites_gist
-  ON "PGHP".limites
+  ON "PGHP_2".limites
   USING gist
   (geom);
+*/
