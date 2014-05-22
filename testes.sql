a1fe83bec68c4b8cc4fe43d924afcdf55c2a94e6
SrNetoChan <senhor.neto@gmail.com>
2014-05-22 18:24:20 +0100

    add copyright

--- testes.sql
+++ testes.sql
@@ -17,6 +17,7 @@ SELECT gid, first(descr) as descr, first(geom) as geom, first(vrs_start_time) as
 FROM all_tables
 GROUP BY gid
 ORDER BY gid
+
 SELECT array_to_string(ARRAY(SELECT 'first(g.' || c.column_name || ') as ' || c.column_name
 
 SELECT array_to_string(ARRAY(SELECT 'first(g.' || c.column_name || ') as ' || c.column_name
@@ -27,3 +28,19 @@ SELECT array_to_string(ARRAY(SELECT 'first(g.' || c.column_name || ') as ' || c.
 SELECT array_to_string(ARRAY(SELECT 'first(g.' || c.column_name || ') as ' || c.column_name
 			FROM information_schema.columns As c
 			WHERE table_schema = 'PGHP_2' and table_name = 'testes_versioning'), ', ')
+
+
+SELECT * FROM information_schema.columns
+WHERE table_name = 'testes_numeric'
+ORDER BY data_type
+
+SELECT * 
+FROM geometry_columns
+
+Select g.column_name, g.data_type, g.character_maximum_length, numeric_precision, numeric_scale, g.udt_name, f.type, f.srid
+from information_schema.columns as g join geometry_columns as f ON (g.table_schema = f.f_table_schema and g.table_name = f.f_table_name )
+where table_schema = 'PGHP_2' and table_name = 'testes_versioning'
+
+Create table testes_numeric
+(gid serial,
+numericvalue numeric (15,5))
