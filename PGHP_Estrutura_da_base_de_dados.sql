617ba49aca593869e38385655dd7a89c2c13c5c2
SrNetoChan <senhor.neto@gmail.com>
2014-07-24 09:30:02 +0100

    versao completa da estrutura de dados

--- PGHP_Estrutura_da_base_de_dados.sql
+++ PGHP_Estrutura_da_base_de_dados.sql
@@ -4,14 +4,19 @@
   DROP TABLE IF EXISTS "PGHP_2".unidadesdegestao_bk CASCADE;
   DROP TABLE IF EXISTS "PGHP_2".unidadesterritoriais_bk CASCADE;
   DROP TABLE IF EXISTS "PGHP_2".unidadesdegestao_linhas_bk CASCADE;
-  DROP TABLE IF EXISTS "PGHP_2".unidadesdegestao_poligonos_bk CASCADE; */
+  DROP TABLE IF EXISTS "PGHP_2".unidadesdegestao_poligonos_bk CASCADE;
+  DROP TABLE IF EXISTS "PGHP_2".acao_tipo CASCADE;
+  DROP TABLE IF EXISTS "PGHP_2".acoes CASCADE;
+  DROP TABLE IF EXISTS "PGHP_2".acoes_bk CASCADE;
+  DROP TABLE IF EXISTS "PGHP_2".acoes_detalhe CASCADE;
+   DROP TABLE IF EXISTS "PGHP_2".acoes_detalhe_bk CASCADE;*/
 
 
 -- UNIDADES TERRITORIAIS --
 /** Unidades territoriais representam as macro unidades que devem ser geridas como um todo
     Exemplo disso são Quinta do Pisão, Pedra amarela Campo base, Duna da Cresmina, etc... **/
 
-CREATE TABLE "PGHP_2".unidadesterritoriais
+CREATE TABLE "PGHP".unidadesterritoriais
 (
   gid serial PRIMARY KEY,
   oid serial UNIQUE,
@@ -21,13 +26,13 @@ CREATE TABLE "PGHP_2".unidadesterritoriais
 );
 
 CREATE INDEX unidadesterritoriais_gist
-  ON "PGHP_2".unidadesterritoriais
+  ON "PGHP".unidadesterritoriais
   USING gist
   (geom);
 
 -- Criar versioning --
 
-SELECT vsr_add_versioning_to('"PGHP_2".unidadesterritoriais');
+SELECT vsr_add_versioning_to('"PGHP".unidadesterritoriais');
 
 -- UNIDADES DE GESTAO --
 /** Unidades de gestão representam áreas homogéneas tanto em termos de proposta de ocupação,
@@ -35,89 +40,90 @@ SELECT vsr_add_versioning_to('"PGHP_2".unidadesterritoriais');
     de forma concertada entre os vários interveniente e responsáveis antes de se definir acções **/
 
 -- Criação de tabelas auxiliares referente aos possíveis usos do solo
-CREATE TABLE "PGHP_2".usos (
+DROP TABLE "PGHP".usos
+CREATE TABLE "PGHP".usos (
   oid serial PRIMARY KEY,
-  nome character varying(30) UNIQUE NOT NULL
+  nome character varying(30) UNIQUE
 );
 
 -- tabela de unidades de gestão geral (parent)
-CREATE TABLE "PGHP_2".unidadesdegestao
+CREATE TABLE "PGHP".unidadesdegestao
 (
   gid serial PRIMARY KEY,
   oid serial UNIQUE NOT NULL,
-  uniterr_oid integer REFERENCES "PGHP_2".unidadesterritoriais(oid) ON UPDATE CASCADE ON DELETE RESTRICT,
+  uniterr_oid integer REFERENCES "PGHP".unidadesterritoriais(oid) ON UPDATE CASCADE ON DELETE RESTRICT,
   nome character varying(40),
-  act_uso character varying(30) REFERENCES "PGHP_2".usos(nome) ON UPDATE CASCADE ON DELETE RESTRICT,
+  act_uso character varying(30) REFERENCES "PGHP".usos(nome) ON UPDATE CASCADE ON DELETE RESTRICT,
   act_composicao character varying(50),
-  prop_uso character varying(30) REFERENCES "PGHP_2".usos(nome) ON UPDATE CASCADE ON DELETE RESTRICT,
+  prop_uso character varying(30) REFERENCES "PGHP".usos(nome) ON UPDATE CASCADE ON DELETE RESTRICT,
   prop_composicao character varying(50),
   prop_data_limite timestamp without time zone,
   geom geometry(geometry, 3763)
 );
 
 CREATE INDEX unidadesdegestao_idx
-  ON "PGHP_2".unidadesdegestao
+  ON "PGHP".unidadesdegestao
   USING gist
   (geom);
 
 -- Tabela inherits das unidades de gestao para geometrias de linhas (child)
-CREATE TABLE "PGHP_2".unidadesdegestao_linhas
+CREATE TABLE "PGHP".unidadesdegestao_linhas
 (
 	-- colocar constraint na geometria para aceitar só linhas
 	CONSTRAINT enforce_geotype_geom CHECK (geometrytype(geom) = 'MULTILINESTRING'::text),
 	-- constraints da tabela parent têm de ser refeitas nas tabelas child
 	CONSTRAINT unidadesdegestao_linhas_pk PRIMARY KEY (gid),
 	CONSTRAINT unidadesdegestao_linhas_oid_key UNIQUE (oid),
-	CONSTRAINT unidadesdegestao_unidadesterritoriais_fkey FOREIGN KEY (uniterr_oid) REFERENCES "PGHP_2".unidadesterritoriais(oid) ON UPDATE CASCADE ON DELETE RESTRICT,
-	CONSTRAINT unidadesdegestao_linhas_act_uso_fkey FOREIGN KEY (act_uso) REFERENCES "PGHP_2".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT,
-	CONSTRAINT unidadesdegestao_linhas_prop_uso_fkey FOREIGN KEY (prop_uso) REFERENCES "PGHP_2".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT
+	CONSTRAINT unidadesdegestao_unidadesterritoriais_fkey FOREIGN KEY (uniterr_oid) REFERENCES "PGHP".unidadesterritoriais(oid) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT unidadesdegestao_linhas_act_uso_fkey FOREIGN KEY (act_uso) REFERENCES "PGHP".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT unidadesdegestao_linhas_prop_uso_fkey FOREIGN KEY (prop_uso) REFERENCES "PGHP".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT
 )
-	INHERITS ("PGHP_2".unidadesdegestao);
+	INHERITS ("PGHP".unidadesdegestao);
 
 -- Em tabelas child os índices têm de ser recriados
 CREATE INDEX unidadesdegestao_linhas_idx
-  ON "PGHP_2".unidadesdegestao_linhas
+  ON "PGHP".unidadesdegestao_linhas
   USING gist
   (geom);
 
 -- Criar versioning --
 
-SELECT vsr_add_versioning_to('"PGHP_2".unidadesdegestao_linhas');
+SELECT vsr_add_versioning_to('"PGHP".unidadesdegestao_linhas');
 
 -- Tabela das unidades de gestao para geometrias de polígonos (child)
-CREATE TABLE "PGHP_2".unidadesdegestao_poligonos
+CREATE TABLE "PGHP".unidadesdegestao_poligonos
 (
 	-- colocar constraint na geometria para aceitar só linhas
 	CONSTRAINT enforce_geotype_geom CHECK (geometrytype(geom) = 'MULTIPOLYGON'::text),
 	-- constraints da tabela parent têm de ser refeitas nas tabelas child
 	CONSTRAINT unidadesdegestao_poligonos_pk PRIMARY KEY (gid),
 	CONSTRAINT unidadesdegestao_poligonos_oid_key UNIQUE (oid),
-	CONSTRAINT unidadesdegestao_unidadesterritoriais_fkey FOREIGN KEY (uniterr_oid) REFERENCES "PGHP_2".unidadesterritoriais(oid) ON UPDATE CASCADE ON DELETE RESTRICT,
-	CONSTRAINT unidadesdegestao_linhas_act_uso_fkey FOREIGN KEY (act_uso) REFERENCES "PGHP_2".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT,
-	CONSTRAINT unidadesdegestao_linhas_prop_uso_fkey FOREIGN KEY (prop_uso) REFERENCES "PGHP_2".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT
+	CONSTRAINT unidadesdegestao_unidadesterritoriais_fkey FOREIGN KEY (uniterr_oid) REFERENCES "PGHP".unidadesterritoriais(oid) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT unidadesdegestao_linhas_act_uso_fkey FOREIGN KEY (act_uso) REFERENCES "PGHP".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CONSTRAINT unidadesdegestao_linhas_prop_uso_fkey FOREIGN KEY (prop_uso) REFERENCES "PGHP".usos (nome) ON UPDATE CASCADE ON DELETE RESTRICT
 )
-	INHERITS ("PGHP_2".unidadesdegestao);
+	INHERITS ("PGHP".unidadesdegestao);
 
 -- Em tabelas child os índices têm de ser recriados
 CREATE INDEX unidadesdegestao_poligonos_idx
-  ON "PGHP_2".unidadesdegestao_poligonos
+  ON "PGHP".unidadesdegestao_poligonos
   USING gist
   (geom);
 
 -- Criar versioning --
 
-SELECT vsr_add_versioning_to('"PGHP_2".unidadesdegestao_poligonos');
+SELECT vsr_add_versioning_to('"PGHP".unidadesdegestao_poligonos');
 
 -- Workaround to assign default sequential value to an empty oid field instead of Zero
 -- Useful when the feature geometry is split and you want to keep one of the oid to one part and default values to the others
 
 -- Function to replace oid zero values
-CREATE OR REPLACE FUNCTION "PGHP_2".update_oid()
+CREATE OR REPLACE FUNCTION "PGHP".update_oid()
 RETURNS trigger AS
 $$
 BEGIN
 	IF NEW.oid = 0 THEN
-		NEW.oid = nextval('"PGHP_2".unidadesdegestao_oid_seq'::regclass);
+		NEW.oid = nextval('"PGHP".unidadesdegestao_oid_seq'::regclass);
 	END IF;
 	RETURN NEW;
 END;
@@ -126,12 +132,74 @@ LANGUAGE 'plpgsql';
 
 -- Trigger Function to replace oid zero values
 CREATE TRIGGER update_oid_trg
-  BEFORE INSERT OR UPDATE OF oid ON "PGHP_2".unidadesdegestao_linhas
+  BEFORE INSERT OR UPDATE OF oid ON "PGHP".unidadesdegestao_linhas
   FOR EACH ROW
-  EXECUTE PROCEDURE "PGHP_2".update_oid();
+  EXECUTE PROCEDURE "PGHP".update_oid();
 
 CREATE TRIGGER update_oid_trg
-  BEFORE INSERT OR UPDATE OF oid ON "PGHP_2".unidadesdegestao_poligonos
+  BEFORE INSERT OR UPDATE OF oid ON "PGHP".unidadesdegestao_poligonos
   FOR EACH ROW
-  EXECUTE PROCEDURE "PGHP_2".update_oid();
+  EXECUTE PROCEDURE "PGHP".update_oid();
 
+
+-- ACÇÕES --
+
+-- tabelas auxiliares --  
+
+CREATE TABLE "PGHP".acao_tipo (
+  oid SERIAL PRIMARY KEY,
+  nome character varying(40) UNIQUE
+);
+
+-- tabela espacial
+CREATE TABLE "PGHP".acoes_detalhe (
+  gid SERIAL PRIMARY KEY,
+  geom geometry(MultiPolygon,3763)
+);
+
+-- Criar versioning --
+
+SELECT vsr_add_versioning_to('"PGHP".acoes_detalhe');
+
+-- tabela das accoes
+CREATE TABLE "PGHP".acoes(
+  gid SERIAL PRIMARY KEY,
+  data_prev_inicio timestamp without time zone,
+  data_prev_fim timestamp without time zone,
+  uniges_oid INTEGER REFERENCES "PGHP".unidadesdegestao(oid) ON UPDATE CASCADE ON DELETE RESTRICT,
+  tipo character varying(40) REFERENCES "PGHP".acao_tipo(nome) ON UPDATE CASCADE ON DELETE RESTRICT,
+  descricao character varying(255),
+  entidade character varying(30),
+  responsavel character varying(30),
+  custo numeric(20,2),
+  areas_detalhe_gid integer REFERENCES "PGHP".acoes_detalhe(gid) ON UPDATE CASCADE ON DELETE RESTRICT,
+  data_exec_inicio timestamp without time zone,
+  data_exec_fim timestamp without time zone
+);
+
+-- Criar versioning --
+
+SELECT vsr_add_versioning_to('"PGHP".acoes');
+
+-- View para espacializar as acções
+CREATE OR REPLACE VIEW "PGHP".geo_acoes AS 
+ SELECT a.gid, a.data_prev_inicio, a.data_prev_fim, a.uniges_oid, a.tipo, a.descricao, 
+    a.entidade, a.responsavel, a.custo, a.areas_detalhe_gid, a.data_exec_inicio, a.data_exec_fim,
+        CASE
+            WHEN a.areas_detalhe_gid IS NULL THEN u.geom
+            ELSE d.geom
+        END AS geom
+   FROM "PGHP".acoes a
+   LEFT JOIN "PGHP".acoes_detalhe d ON a.areas_detalhe_gid = d.gid
+   LEFT JOIN "PGHP".unidadesdegestao u ON a.uniges_oid = u.oid;
+
+-- ::FIXME 
+
+
+-- Siglas para composição
+CREATE TABLE "PGHP".uso_siglas (
+gid SERIAL PRIMARY KEY,
+tipo varchar(10),
+sigla varchar(5),
+nome varchar(40)
+);
--- versioning_example.sql
+++ versioning_example.sql
@@ -28,3 +28,81 @@ SELECT * from vsr_table_at_time (NULL::"PGHP_2".testes_versioning, '2014-06-04 1
 
 -- See specific feature at certain time
 SELECT * from vsr_table_at_time (NULL::"PGHP_2".testes_versioning, '2014-04-19 18:26:57') WHERE gid = 1;
+
+
+
+WITH g as
+(
+SELECT gid, descr, valor, valor_dec, geom FROM "PGHP_2".testes_versioning_bk WHERE vrs_start_time <= '2014-06-11 11:26:0' and vrs_end_time >= '2014-06-11 11:26:0'
+UNION ALL
+SELECT gid, descr, valor, valor_dec, geom FROM "PGHP_2".testes_versioning WHERE vrs_start_time <= '2014-06-11 11:26:0'
+UNION ALL
+SELECT gid, descr, valor, valor_dec, geom FROM "PGHP_2".testes_versioning_bk WHERE vrs_end_time >= '2014-06-11 11:26:0'
+)
+SELECT gid, first(descr), first(valor), first(valor_dec), first(geom)
+FROM g
+GROUP BY gid
+
+with actual as
+(
+SELECT
+	gid, descr, valor, valor_dec, geom, vrs_start_time
+FROM
+	"PGHP_2".testes_versioning
+WHERE vrs_start_time <= '2014-06-12 11:01:00'
+),
+incomplete as
+(
+SELECT 
+	gid, descr, valor, valor_dec, geom, vrs_start_time
+FROM
+	"PGHP_2".testes_versioning_bk
+WHERE
+	vrs_start_time <= '2014-06-12 11:01:00' and vrs_end_time >= '2014-06-12 11:01:0'
+),
+old_backup as
+(
+SELECT 
+	gid, descr, valor, valor_dec, geom, vrs_start_time
+FROM
+	"PGHP_2".testes_versioning_bk
+WHERE 
+	gid in (SELECT distinct gid from incomplete)
+),
+old_table as
+(
+SELECT 
+	gid, descr, valor, valor_dec, geom, vrs_start_time
+FROM
+	"PGHP_2".testes_versioning
+WHERE
+	gid in (SELECT distinct gid from incomplete)
+)
+SELECT * FROM actual
+UNION ALL
+SELECT * FROM incomplete
+UNION ALL
+SELECT * FROM old_backup
+UNION ALL
+SELECT * FROM old_table
+
+WITH tudo as
+(SELECT 
+	gid, descr, valor, valor_dec, geom, vrs_start_time, vrs_end_time
+ FROM
+	"PGHP_2".testes_versioning_bk
+UNION ALL
+ SELECT
+	gid, descr, valor, valor_dec, geom, vrs_start_time,NULL
+ FROM "PGHP_2".testes_versioning
+)
+SELECT gid, descr, valor, valor_dec, geom, vrs_start_time, vrs_end_time
+FROM tudo
+WHERE gid in (SELECT DISTINCT gid
+		FROM tudo
+		WHERE vrs_start_time <= '2014-06-12 11:21:00' and (vrs_end_time >= '2014-06-12 11:21:0' OR vrs_end_time IS NULL))
+GROUP BY gid
+
+
+
+
