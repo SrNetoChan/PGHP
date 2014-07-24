d8f957575dac0ace666185ff15421ea466502d8e
SrNetoChan <senhor.neto@gmail.com>
2014-07-24 15:35:29 +0100

    SQL para preencher UG com COS em determinada UT

---
+++ cos_para_unidade_gestao.sql
@@ -0,0 +1,5 @@
+﻿WITH f as (
+SELECT st_transform(geom,3763)::geometry(Multipolygon,3763) as geom
+FROM "PGHP".unidadesterritoriais
+WHERE oid = 4)
+SELECT g.gid as gid_temp, 4 as uniterr_oid, (g.cosn3 || '-' ||t.descricao) as nome, St_Intersection(st_transform(g.geom,3763),f.geom) as geom  FROM cosc.cosc09 as g JOIN f ON (St_Intersects(st_transform(g.geom,3763),f.geom)) JOIN cosc.nomenclatura_cosn3 as t ON (g.cosn3 = t.cosn3)
